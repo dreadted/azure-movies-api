f7042f6ecc7ee5b535163f4f255be0d2a455dfe5
Christofer Laurin <christofer.laurin@gmail.com>
2020-04-27 08:40:29 +0200

    feat(config) : encrypt

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,7 +2,8 @@ const config = {
   user: process.env.DBUser,
   password: process.env.DBPassword,
   server: process.env.DBServer,
-  database: "Movies"
+  database: "Movies",
+  options: { encrypt: true }
 };
 
 export default config;
